a104559856535436fde91d57f9b2d0075db3397e
ITHUBBLR-SYSL-36 <itcoblr@muthootgroup.com>
2024-03-06 15:54:45 +0530

    Applicant details View

--- lib/View/documentsPageView.dart
+++ lib/View/documentsPageView.dart
@@ -40,13 +40,49 @@ class _DocumentPageViewState extends State<DocumentPageView> {
         centerTitle: true,
 
       ),
-      body: Row(
+      body: Column(
         children: [
-          Expanded(
-            child: buildCheckboxColumn('Mandatory', checkboxValues),
+          Column(
+            children: checkboxValues.keys.map((String key) {
+              return Row(
+                children: [
+                  Checkbox(
+                    value: checkboxValues[key],
+                    activeColor: StyleData.appBarColor,
+                    onChanged: (value) {
+                      setState(() {
+                        checkboxValues[key] = value!;
+                      });
+                    },
+                  ),
+                  Text(
+                    key,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              );
+            }).toList(),
           ),
-          Expanded(
-            child: buildCheckboxColumn('Optional', optionalCheckboxes),
+          Column(
+            children: optionalCheckboxes.keys.map((String key) {
+              return Row(
+                children: [
+                  Checkbox(
+                    value: optionalCheckboxes[key],
+                    activeColor: StyleData.appBarColor,
+                    onChanged: (value) {
+                      setState(() {
+                        optionalCheckboxes[key] = value!;
+                      });
+                    },
+                  ),
+                  Text(
+                    key,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              );
+            }).toList(),
           ),
         ],
       ),
@@ -57,40 +93,6 @@ class _DocumentPageViewState extends State<DocumentPageView> {
       //   child: Icon(Icons.save),
       // ),
     )
-    );
-  }
-
-  Widget buildCheckboxColumn(String title, Map<String, bool> checkboxes) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        ...checkboxes.keys.map((String key) {
-          return Row(
-            children: [
-              Checkbox(
-                value: checkboxes[key],
-                activeColor: StyleData.appBarColor,
-                onChanged: (value) {
-                  setState(() {
-                    checkboxes[key] = value!;
-                  });
-                },
-              ),
-              Text(
-                key,
-                style: TextStyle(fontSize: 18),
-              ),
-            ],
-          );
-        }).toList(),
-      ],
     );
   }
 }
