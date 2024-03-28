19a7eb02c3557c6ddb0140e46136ab89b7f730fa
ITHUBBLR-SYSL-36 <itcoblr@muthootgroup.com>
2024-03-28 12:21:39 +0530

    removed UDID

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'View/SplashView.dart';
-import 'View/UDIDScreen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
